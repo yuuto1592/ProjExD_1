7a0463076442d77d217fe556cc0952e8cdd9cca7
yuuto1592 <haya.you0323@gmail.com>
2026-06-09 16:13:23 +0900

    こうかとんの操作

--- pygame_image.py
+++ pygame_image.py
@@ -14,15 +14,27 @@ def main():
     tmr = 0
     koukaton_img=pg.image.load("fig/3.png")
     koukaton_img=pg.transform.flip(koukaton_img,True,False)
+    koukaton_rct=koukaton_img.get_rect()
+    koukaton_rct.center=300,200
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
+        key_lst=pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            koukaton_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            koukaton_rct.move_ip((0,1))
+        if key_lst[pg.K_RIGHT]:
+            koukaton_rct.move_ip((1,0))
+        if key_lst[pg.K_LEFT]:
+            koukaton_rct.move_ip((-1,0))
+
         x=tmr%3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(turn_bg_img, [-x+1600, 0])
         screen.blit(bg_img,[-x+3200,0])
-        screen.blit(koukaton_img,[300,200])
+        screen.blit(koukaton_img,koukaton_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
