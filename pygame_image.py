0abb187036596760122049b6120c2ba0afd67e2e
yuuto1592 <haya.you0323@gmail.com>
2026-06-09 15:43:15 +0900

    背景画像の更新

--- pygame_image.py
+++ pygame_image.py
@@ -17,11 +17,12 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x=tmr
+        screen.blit(bg_img, [-x, 0])
         screen.blit(koukaton_img,[300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
